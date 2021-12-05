48c289964d8e90347ffe6e85252b82a91aeadfe3
Frank Herrala <frank.herrala@tuni.fi>
2021-12-05 12:14:20 +0200

    Words tests created

--- src/__tests__/words.test.js
+++ src/__tests__/words.test.js
@@ -7,3 +7,23 @@ test('Test string without pattern', () => {
 test('Test string with pattern', () => {
     expect(words("String with,& too",/[^, ]+/g)).toStrictEqual(["String", "with", "&", "too"]);
 })
+
+test('Test asciiWords match words', () => {
+    expect(words('Aa Bb')).toStrictEqual(['Aa', 'Bb']);
+})
+
+test('Test empty', () => {
+    expect(words('')).toStrictEqual([]);
+})
+
+test('Test empty with pattern', () => {
+    expect(words('', /[^, ]+/g)).toStrictEqual([]);
+})
+
+test('Test with found string pattern', () => {
+    expect(words('test other', 'other')).toStrictEqual(['other']);
+})
+
+test('Test with unfound string pattern', () => {
+    expect(words('test other', 'nomatch')).toStrictEqual([]);
+})
