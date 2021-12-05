15e4671939176486003336c6f48e7c172dd4748e
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-12-05 10:17:58 +0200

    Add tests for filter.js (#4)

--- src/__tests__/filter.test.js
+++ src/__tests__/filter.test.js
@@ -7,4 +7,16 @@ describe('filter function', () => {
     it('should return array with size of one when filter matches exact string', () => {
         expect(filter(stringArray, s => s=='cat')).toEqual(['cat'])
     })
+    it('should return array in array when null is given as parameter', () => {
+        expect(filter(null)).toEqual([[]])
+    })
+    it('should return array in array when no parameter is given', () => {
+        expect(filter(null)).toEqual([[]])
+    })
+    it('should throw error when array is given as parameter but no function parameter is given as predicate', () => {
+        expect(() => filter([[]])).toThrow()
+    })
+    it('should throw error when array is given as parameter and integer is given as predicate', () => {
+        expect(() => filter([[]], 1)).toThrow()
+    })
 })
