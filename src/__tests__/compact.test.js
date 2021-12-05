32823f1ca94691f5bbd06064e76277913c225271
Frank Herrala <frank.herrala@tuni.fi>
2021-12-05 13:53:15 +0200

    Added compact tests

--- src/__tests__/compact.test.js
+++ src/__tests__/compact.test.js
@@ -1,6 +1,13 @@
 import compact from '../compact';
 
-test('Test', () => {
-    function test (){return true};
-    expect(test()).toBe(true);
+test('Test with boolean, integer, empty string, null, undefined and NaN', () => {
+    expect(compact([0, 1, false, 2, '', 3, undefined, null, NaN])).toBe([1, 2, 3]);
+})
+
+test('Test with boolean, integer and string', () => {
+    expect(compact([0, 1, false, 2, 'string', 3])).toBe([1, 2, 'string', 3]);
+})
+
+test('Test with objects', () => {
+    expect(compact([false, null, {value: 1}, {value: null}])).toBe([{value: 1}, {value: null}]);
 })
